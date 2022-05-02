984f8bc218bc71e1b70024ba774f2eab6d391f95
Karim Khasan <karimhasan3879@gmail.com>
2022-05-02 16:19:40 +0300

    Added test cases and fixed functions

--- Lab6/src/compiletime_polynomial.hpp
+++ Lab6/src/compiletime_polynomial.hpp
@@ -1,97 +1,242 @@
 #ifndef COMPILETIME_POLYNOMIAL_HPP
 #define COMPILETIME_POLYNOMIAL_HPP
 #include <iostream>
-//////template<int ...Args>
-//////class ICTPolynomial {
-//////public:
-//////	virtual ~ICTPolynomial() {}
-//////	virtual void print() = 0;
-//////};
-//////
-//////template<int ...Args>
-//////class CTPolynomial : ICTPolynomial {};
+#include "polynomial.hpp"
 
+// Объявление шаблонного класса полинома
 template<int ...Args>
 class CTPolynomial;
 
-template<int ...Args>
+// Объявление метафункции, рассчитывающей значение полинома в точке
+template<int V, typename Polynomial>
 class Evaluation;
 
+// Объявление метафункции, рассчитывающей значение производной от функции, заданной полиномом, в точке
+template<int Value, int Degree, typename Polynomial>
+class Derivative;
 
+// Объявление метафункции, рассчитывающей значение определённого интеграла
+// от функции, заданной полиномом, в соответствии с заданными пределами интегрирования
+template<int LowerBound, int UpperBound, int Degree, typename Polynomial>
+class Integral;
+
+// Метафункция, рассчитывающая степень полинома
 template<typename Polynomial>
 class Degree {
 public:
 	static const int value = 1 + Degree<typename Polynomial::Shifted>::value;
 };
 
+// Частная специализация шаблонного класса Degree - база для случая пустого полинома
 template<>
 class Degree<CTPolynomial<>> {
 public:
+	// Степень пустого полинома = -1
 	static const int value = -1;
 };
 
+
+// Частная специализация класса полинома для случая без коэффициентов
 template<>
 class CTPolynomial<> {
 public:
 	using type = CTPolynomial<>;
-	static consteval int evaluation(int) { return 0; }
-	static consteval int degree() {
-		return Degree<type>::value;
+	static consteval long long evaluation(int) { return 0; }
+	static consteval int degree() { return -1; }
+	static consteval long long derivative(int value) { return 0; }
+	static consteval double integral(int, int) { return 0; }
+	static void print(std::ostream& os, int deg = -1, bool is_first_element = true) { 
+		if (deg == -1) {
+			os << 0;
+		}
+	}
+	operator CPolynomial<int>() const {
+		return CPolynomial<int>();
 	}
 };
 
+// Частная специализация класса Evaluation для случая пустого полинома - база рекурсии
 template<int V>
-class Evaluation<V> {
+class Evaluation<V, CTPolynomial<>> {
 public:
-	static const int value = 0;
+	static const long long value = 0;
 };
 
+// Частная специализация класса Derivative для случая пустого полинома - база рекурсии
+template<int V, int Degree>
+class Derivative<V, Degree, CTPolynomial<>> {
+public:
+	static const long long value = 0;
+};
 
+// Частная специализация класса Integral для случая пустого полинома - база рекурсии
+template<int LowerBound, int UpperBound, int Degree>
+class Integral<LowerBound, UpperBound, Degree, CTPolynomial<>> {
+public:
+	inline static const double value = 0;
+};
+
+// Частная специализация класса CTPolynomial для полинома с коэффициентами
 template<int FirstElement, int... Rest>
 class CTPolynomial<FirstElement, Rest...> {
 public:
 	using type = CTPolynomial<FirstElement, Rest...>;
 	using Shifted = CTPolynomial<Rest...>;
-	static consteval int degree() {
-		return Degree<type>::value;
-	}
-	static consteval int evaluation(int a) {
-		int eval = FirstElement;
-		for (const auto coef : { Rest... }) {
-			eval += coef * a;
-			a *= a;
+	static consteval int degree() { return sizeof...(Rest); }
+	static consteval long long derivative(int value) {
+		if constexpr (sizeof...(Rest) == 0) {
+			return 0;
 		}
-		return eval;
+		else {
+			int eval = 0;
+			int degree = 1;
+			int value_mult = value;
+			for (auto const coef : { Rest... }) {
+				if (degree == 1) {
+					eval = coef;
+				}
+				else {
+					eval += coef * value * degree;
+					value *= value_mult;
+				}
+				++degree;
+			}
+			return eval;
+		}
+	}
+	static consteval long long evaluation(int a) {
+		if constexpr (sizeof...(Rest) == 0) {
+			return FirstElement;
+		}
+		else {
+			int eval = FirstElement;
+			for (const auto coef : { Rest... }) {
+				eval += coef * a;
+				a *= a;
+			}
+			return eval;
+		}
+	}
+	static consteval double integral(int lower_bound, int upper_bound) {
+		double result = (upper_bound - lower_bound) * FirstElement;
+		if constexpr (sizeof...(Rest) == 0) {
+			return result;
+		}
+		else {
+			size_t degree = 2;
+			int upper_mult = upper_bound, lower_mult = lower_bound;
+			for (const auto coef : { Rest... }) {
+				upper_bound *= upper_mult;
+				lower_bound *= lower_mult;
+				result += (upper_bound - lower_bound) * static_cast<double>(coef) / degree;
+				++degree;
+			}
+			return result;
+		}
+	}
+	static void print(std::ostream &os, int deg = degree(), bool is_first_element = true) {
+		int relative_degree = deg - degree();
+		if (FirstElement != 0) {
+			if (relative_degree == 0) {
+				os << FirstElement;
+				is_first_element = false;
+			}
+			else {
+				if (is_first_element) {
+					is_first_element = false;
+				}
+				else {
+					os << " + ";
+				}
+				os << FirstElement << "x";
+			}
+			if (relative_degree > 1) {
+				os << "^" << relative_degree;
+			}
+		}
+		Shifted::print(os, deg, is_first_element);
+	}
+	operator CPolynomial<int>() const {
+		return CPolynomial<int>{ FirstElement, Rest... };
 	}
 };
 
-
+// Частная специализация класса Evaluation для полинома с коэффициентами
 template<int V, int FirstElement, int... Rest>
-class Evaluation<V, FirstElement, Rest...> {
+class Evaluation<V, CTPolynomial<FirstElement, Rest...>> {
 public:
-	static const int value = FirstElement + V * Evaluation<V, Rest...>::value;
+	static const long long value = FirstElement + V * Evaluation<V, CTPolynomial<Rest...>>::value;
 };
 
+// Частная специализация класса Derivative для полинома с коэффициентами
+template<int V, int Degree, int FirstElement, int... Rest>
+class Derivative<V, Degree, CTPolynomial<FirstElement, Rest...>> {
+public:
+	static const long long value = (Degree - sizeof...(Rest)) * FirstElement \
+		+ V * Derivative<V, Degree, CTPolynomial<Rest...>>::value;
+};
 
-std::ostream& operator<<(std::ostream& os, CTPolynomial<> const& poly) {
-	os << 0;
+// Метафункция Pow для возведения числа в неотрицательную степень
+template<int Value, int Degree>
+class Pow {
+public:
+	static const long long value = Value * Pow<Value, Degree - 1>::value;
+};
+
+// Частная специализация класса Pow для возведения в нулевую степень - база рекурсии
+template<int Value>
+class Pow<Value, 0> {
+public:
+	static const long long value = 1;
+};
+
+// Частная специализация класса Integral для полинома с коэффициентами
+template<int LowerBound, int UpperBound, int Degree, int FirstElement, int... Rest>
+class Integral<LowerBound, UpperBound, Degree, CTPolynomial<FirstElement, Rest...>> {
+public:
+	inline static const double value = static_cast<double>(FirstElement) / (Degree - sizeof...(Rest) + 1) * \
+		(Pow<UpperBound, (Degree - sizeof...(Rest) + 1)>::value  - Pow<LowerBound, (Degree - sizeof...(Rest) + 1)>::value) + \
+		Integral<LowerBound, UpperBound, Degree, CTPolynomial<Rest...>>::value;
+};
+
+// Перегрузка оператора вывода в поток полинома
+template<int... Args>
+std::ostream& operator<<(std::ostream& os, CTPolynomial<Args...> const& poly) {
+	poly.print(os);
 	return os;
 }
 
-template<int FirstElement, int... Rest>
-std::ostream& operator<<(std::ostream& os, CTPolynomial<FirstElement, Rest...> const& poly) {
-	os << FirstElement;
-	size_t degree = 1;
-	for (const auto coef : { Rest... }) {
-		if (coef != 0) {
-			os << " + " << coef << "x";
-		}
-		if (degree != 1) {
-			os << "^" << degree;
-		}
-		++degree;
+// Внешняя функция вывода в поток для полинома
+template<int... Args>
+void print(std::ostream& os, CTPolynomial<Args...> const& poly) {
+	if constexpr (sizeof...(Args) == 0) {
+		os << 0;
 	}
-	return os;
+	else {
+		size_t degree = 0;
+		bool is_first_element = true;
+		for (const auto coef : { Args... }) {
+			if (coef != 0) {
+				if (degree != 0) {
+					if (is_first_element) {
+						is_first_element = false;
+					}
+					else {
+						os << " + ";
+					}
+					os << coef << "x";
+				}
+				else {
+					is_first_element = false;
+					os << coef;
+				}
+				if (degree > 1) {
+					os << "^" << degree;
+				}
+			}
+			++degree;
+		}
+	}
 }
 
 
--- Lab6/test/test.cpp
+++ Lab6/test/test.cpp
@@ -3,83 +3,117 @@
 #include <fstream>
 #include <sstream>
 #include <string>
+#include <map>
 
-TEST(BasicTest, always_true) {
-    EXPECT_EQ(1, 1);
-    EXPECT_TRUE(true);
+
+TEST(PolynomialInitialization, EmptyInitialized) {
+    CPolynomial<int> All_my_plans_fell_through_my_hand_They_fell_through_my_hands_on_me;
+    Degree<CTPolynomial<>> All_my_dreams_it_suddenly_seems_It_suddenly_seems_empty;
+    CTPolynomial Empty_i_i_i_i_i_i_Empty_i_i_i_i_i_i_i_i;
+    EXPECT_EQ(Empty_i_i_i_i_i_i_Empty_i_i_i_i_i_i_i_i.degree(), \
+        All_my_dreams_it_suddenly_seems_It_suddenly_seems_empty.value);
+    EXPECT_EQ(All_my_plans_fell_through_my_hand_They_fell_through_my_hands_on_me.deg(), -1);
+    EXPECT_EQ(Empty_i_i_i_i_i_i_Empty_i_i_i_i_i_i_i_i.evaluation(98), 0);
+    EXPECT_EQ((Evaluation<98, CTPolynomial<>>::value), 0);
+    EXPECT_EQ(Empty_i_i_i_i_i_i_Empty_i_i_i_i_i_i_i_i.derivative(2006), 0);
+    EXPECT_EQ((Derivative<2006, -1, CTPolynomial<>>::value), 0);
+    EXPECT_EQ(Empty_i_i_i_i_i_i_Empty_i_i_i_i_i_i_i_i.integral(0, 2), 0);
+    EXPECT_EQ((Integral<0, 2, -1, CTPolynomial<>>::value), 0);
+    std::stringstream res_1, res_2, res_3;
+    res_1 << Empty_i_i_i_i_i_i_Empty_i_i_i_i_i_i_i_i;
+    res_2 << All_my_plans_fell_through_my_hand_They_fell_through_my_hands_on_me;
+    print(res_3, Empty_i_i_i_i_i_i_Empty_i_i_i_i_i_i_i_i);
+    EXPECT_EQ(res_1.str(), res_2.str());
+    EXPECT_EQ(res_2.str(), res_3.str());
+    EXPECT_EQ(res_3.str(), "0");
 }
 
-TEST(PolynomialEvaluation, ResultComparison) {
-    constexpr int val = Evaluation<1, 1, 2, 3, 4, 5>::value;
+
+TEST(PolynomialEvaluation, ConstExprAndTemplateResultComparison) {
+    constexpr int val = Evaluation<1, CTPolynomial<1, 2, 3, 4, 5>>::value;
     CTPolynomial<1, 2, 3, 4, 5> poly;
     static_assert(val == poly.evaluation(1));
-    ASSERT_EQ(val, poly.evaluation(1));
-}
-
-TEST(PolynomialEvaluation, MyPolynomialComparison) {
-    CPolynomial<int> poly({ 98, 2006, 456 });
-    int pt = poly.evaluation(6);
-    constexpr int ct = CTPolynomial<98, 2006, 456>::evaluation(6);
-    constexpr int tt = Evaluation<6, 98, 2006, 456>::value;
-    ASSERT_EQ(pt, ct);
-    ASSERT_EQ(ct, tt);
+    EXPECT_EQ(val, poly.evaluation(1));
 }
 
 
-TEST(PolynomialOutput, StringsComparison) {
+TEST(PolynomialEvaluation, CPolynomialEvaluationComparison) {
     CPolynomial<int> poly({ 98, 2006, 456 });
-    CTPolynomial<98, 2006, 456> ppoly;
-    std::stringstream res_1, res_2;
+    long long pt = poly.evaluation(6);
+    constexpr long long ct = CTPolynomial<98, 2006, 456>::evaluation(6);
+    constexpr long long tt = Evaluation<6, CTPolynomial<98, 2006, 456>>::value;
+    EXPECT_EQ(pt, ct);
+    EXPECT_EQ(ct, tt);
+}
+
+
+TEST(PolynomialDegree, ConstExprAndTemplateDegreeComparison) {
+    CTPolynomial<98, 3, 9, 1, 2, 0, 0, 0, 9> poly;
+    EXPECT_EQ(poly.degree(), Degree<decltype(poly)>::value);
+}
+
+
+TEST(PolynomialDegree, CPolynomialDegreeComparison) {
+    CTPolynomial<98, 3, 9, 1, 2, 0, 0, 0, 0, 0, 0, 19> poly;
+    CPolynomial<int> cpoly("98 + 3x + 9x^2 + x^3 + 2x^4 + 19x^11");
+    EXPECT_EQ(poly.degree(), cpoly.deg());
+    EXPECT_EQ(cpoly.deg(), 11);
+}
+
+
+TEST(PolynomialDerivative, ConstExprAndTemplateDerivativeComparison) {
+    CTPolynomial<1, 2, 3, 4, 5> coefs_filled_poly;
+    CTPolynomial<19> single_coef_poly;
+    EXPECT_EQ(coefs_filled_poly.derivative(1), (Derivative<1, Degree<decltype(coefs_filled_poly)>::value, decltype(coefs_filled_poly)>::value));
+    EXPECT_EQ(single_coef_poly.derivative(18239), (Derivative<12, 0, decltype(single_coef_poly)>::value));
+}
+
+
+TEST(PolynomialDerivative, CPolynomialDerivativeComparison) {
+    CTPolynomial<1, 2, 3, 4, 5> coefs_filled_poly;
+    CPolynomial<int> ccoefs("1 + 2x + 3x^2 + 4x^3 + 5x^4");
+    CPolynomial<int> csingle({ 19 });
+    EXPECT_EQ(coefs_filled_poly.derivative(25), derivative(ccoefs, 1).evaluation(25));
+    EXPECT_EQ((Derivative<55, 0, CTPolynomial<19>>::value), derivative(csingle, 1).evaluation(55));
+}
+
+
+TEST(PolynomialIntegral, IntegralEvaluationCheck) {
+    EXPECT_TRUE((Integral<11, 12, 3, CTPolynomial<1, 2, 5, 7>>::value - CTPolynomial<1, 2, 5, 7>::integral(11, 12)) < EPS);
+    EXPECT_TRUE((CTPolynomial<1, 2, 5, 7>::integral(11, 12) - 11351.916666666) < EPS);
+}
+
+
+TEST(PolynomialOutput, StringStreamsComparison) {
+    CPolynomial<int> poly({ -98, 2006, 0, 0, 456 });
+    CTPolynomial<-98, 2006, 0, 0, 456> ppoly;
+    std::stringstream res_1, res_2, res_3;
     res_1 << poly;
     res_2 << ppoly;
+    print(res_3, ppoly);
     std::cout << ppoly << "\n" << poly << "\n";
-    ASSERT_EQ(res_1.str(), res_2.str());
+    print(std::cout, ppoly);
+    std::cout << "\n";
+    EXPECT_EQ(res_1.str(), res_2.str());
+    EXPECT_EQ(res_2.str(), res_3.str());
+    EXPECT_EQ(res_1.str(), "-98 + 2006x + 456x^4");
 }
 
 
-TEST(PolynomialEvaluation, EmptyInitialized) {
-    CTPolynomial All_my_plans_fell_through_my_hand_They_fell_through_my_hands_on_me;
-    Degree<CTPolynomial<>> All_my_dreams_it_suddenly_seems_It_suddenly_seems_empty;
-    CPolynomial<int> Empty_i_i_i_i_i_i_Empty_i_i_i_i_i_i_i_i;
-    ASSERT_EQ(All_my_plans_fell_through_my_hand_They_fell_through_my_hands_on_me.degree(), \
-        All_my_dreams_it_suddenly_seems_It_suddenly_seems_empty.value);
-    ASSERT_EQ(Empty_i_i_i_i_i_i_Empty_i_i_i_i_i_i_i_i.deg(), -1);
+TEST(PolynomialConversion, ConversionTest) {
+    CTPolynomial<1, 2, 0, 3, 4, 0, 12, 87> poly;
+    CPolynomial<int> cpoly = poly;
+    EXPECT_TRUE(cpoly == poly);
+    EXPECT_EQ(poly.degree(), cpoly.deg());
+    EXPECT_EQ((Evaluation<11, decltype(poly)>::value), cpoly.evaluation(11));
+    EXPECT_EQ(poly.derivative(4), derivative(cpoly, 1).evaluation(4));
+    EXPECT_TRUE((Integral<0, 2, 7, decltype(poly)>::value - 3047.03) < EPS);
+    std::stringstream res_1, res_2;
+    res_1 << poly;
+    res_2 << cpoly;
+    EXPECT_EQ(res_1.str(), res_2.str());
 }
 
-//TEST(TestCaseName, TestName) {
-//  EXPECT_EQ(1, 1);
-//  EXPECT_TRUE(true);
-//}
-//
-//
-//TEST(case_1, test_1) {
-//    std::cout << "Hello World!\n";
-//    //std::cout << Degree<CTCEPolynomial<1, 2, 3, 4, 5, 6>>::value << "\n";
-//    constexpr int val = Degree<CTCEPolynomial<1, 2, 3, 4, 5, 6>>::value;
-//    constexpr int another = CTCEPolynomial<1, 2, 3>::evaluation(5);
-//    constexpr int one_more = CTCEPolynomial<1, 2, 3>::evaluation(-10);
-//    //static_assert(one_more == 281);
-//    constexpr int another_temp = CTTPolynomial<5, 1, 2, 3>::value;
-//    constexpr int val_temp = Degree<CTTPolynomial<0, 1, 2, 3, 4, 5, 6>>::value;
-//    std::cout << CTTPolynomial<-10, 1, 2, 3>::value << "\n";
-//    constexpr int one_more_temp = CTTPolynomial<-10, 1, 2, 3>::value;
-//    //static_assert(std::is_constant_evaluated());
-//
-//    ASSERT_EQ(val, val_temp);
-//    ASSERT_EQ(another, another_temp);
-//    ASSERT_EQ(one_more, one_more_temp);
-//    //static_assert(another == another_temp);
-//    //static_assert(val == val_temp);
-//    //static_assert(one_more == one_more_temp);
-//    //std::cout << val << " " << another << " " << one_more << "\n";
-//    CTTPolynomial<-10, 1, 2, 3> pol;
-//    CTCEPolynomial<1, 2, 3> pol_;
-//    std::cout << pol << "\n";
-//    std::cout << pol_ << "\n";
-//}
-//////TEST(test_case_1, test_1) {
-//////    std::cout << Degree<CTPolynomial<1>>::value << "\n";
-//////}
 
 int main(int argc, char *argv[]) {
     testing::InitGoogleTest(&argc, argv);
